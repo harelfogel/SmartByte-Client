bf5ae8e96383fdf6fa78753adc76f989c2b5b7c8
Yovel Elad <yuvel.elad@gmail.com>
2023-04-02 20:33:10 +0300

    add rules switch

---
+++ src/components/UI/Switch/RuleSwitch.js
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import classes from "./Switch.module.scss";
+
+const SERVER_URL = 'http://localhost:3001';
+
+
+export const RuleSwitch = (props) => {
+
+
+
+    const [isActive,setIsActive] = React.useState(props.isActive);
+
+    const ontoggleChange = async () => {
+        // console.log("isActive",isActive);
+        await axios.post(`${SERVER_URL}/rules/${props.id}`, {
+            isActive:!isActive
+        })
+        setIsActive(!isActive);
+    }
+
+
+  let switchClasses = [classes.Switch];
+  if (isActive) {
+    switchClasses.push(classes.Checked);
+  }
+  return (
+    <>
+        <label className={switchClasses.join(" ")}>
+            <input type="checkbox" {...props} onChange={ontoggleChange} checked={isActive} />
+            <div />
+        </label>
+  </>
+  )
+}
