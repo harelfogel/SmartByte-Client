6aebb642511868c71f15a8c2fc3bd66c326def7f
harelfogel <fogell06@gmail.com>
2023-04-13 09:43:33 +0300

    adding suggestion service from db and server and reolbe some warnings

--- src/components/Suggestions/suggestions.service.js
+++ src/components/Suggestions/suggestions.service.js
@@ -1,5 +1,6 @@
+import axios from 'axios';
 
-
+const SERVER_URL = 'http://localhost:3001';
 
 const temperatureMap = {
     1: 15,
@@ -7,7 +8,6 @@ const temperatureMap = {
     3: 27,
     4: 35
 }
-
 
 export const generateRule = (suggestion) => {
     const { device, evidence, state } = suggestion;
@@ -20,7 +20,17 @@ export const generateRule = (suggestion) => {
     const action = `("${device} ${state}")`;
 
     const generatedRule = `IF ${conditions} THEN TURN${action}`;
-    console.log({generatedRule})
+    console.log({ generatedRule })
     return generatedRule;
 
-}   
+}  
+
+export const getSuggestions = async () => {
+    try {
+      const response = await axios.get(`${SERVER_URL}/suggestions`);
+      return response.data.data;
+    } catch (error) {
+      console.error('Error fetching suggestions:', error);
+      return [];
+    }
+  };
