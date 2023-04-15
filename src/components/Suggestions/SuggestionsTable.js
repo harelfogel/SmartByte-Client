3d6d5ef52bc185c1f3a6e2d43a7a1e5b62a52f2f
Yovel Elad <yuvel.elad@gmail.com>
2023-04-15 11:47:36 +0300

    move style to external file

--- src/components/Suggestions/SuggestionsTable.js
+++ src/components/Suggestions/SuggestionsTable.js
@@ -5,116 +5,16 @@ import { getSuggestions, updateSuggestions } from "./suggestions.service";
 import styled from "styled-components";
 import { RuleCell } from "./RuleCell";
 import { generateRule } from "./suggestions.service";
-
-const TableContainer = styled.div`
-  display: flex;
-  width: 80%;
-  margin: auto;
-  justify-content: center;
-  padding-top: 4rem;
-  flex-direction: column;
-`;
-
-const TableStyled = styled.table`
-  //   border-collapse: collapse;
-  width: 80%;
-  margin-bottom: 2rem;
-  margin: auto;
-`;
-
-const ThStyled = styled.th`
-  //   border: 1px solid #ccc;
-  padding: 0.5rem 1.5rem;
-  text-align: left;
-`;
-
-const TdStyled = styled.td`
-  //   border: 1px solid #ccc;
-  max-width: 100px;
-  padding: 0.5rem 1.5rem;
-  text-align: left;
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  border-bottom: 1px solid #ccc;
-  color: gray;
-  flex-direction: row;
-  // display: inline-block;
-`;
-
-// const DeviceCellContent = styled.td`
-//   //   border: 1px solid #ccc;
-//   min-width: 100px;
-//   padding: 0.5rem 1.5rem;
-//   text-align: left;
-//   white-space: nowrap;
-//   overflow: hidden;
-//   text-overflow: ellipsis;
-//   border-bottom: 1px solid #ccc;
-//   color: gray;
-//   flex-direction: row;
-//   display: flex;
-//   flex-wrap: space-around;
-//   width: 200px;
-//   position: relative;
-// `;
-
-const DeviceCellContent = styled.div`
-  display: flex;
-  width: 100%;
-  flex-direction: row;
-  justify-content: space-between;
-`;
-
-// const RuleCell = styled.div`
-// // max-width: 100px;
-// text-align: left;
-// white-space: nowrap;
-// overflow: hidden;
-// text-overflow: ellipsis;
-
-// `;
-
-const TitleStyled = styled.p`
-  font-size: 1.5rem;
-`;
-
-const TableContent = styled.div`
-  width: 100%;
-  justify-content: center;
-  align-items: center;
-  background-color: green;
-`;
-
-export const NewTag = styled.div`
-  background-color: #ac98ff;
-  width: 4rem;
-  height: 1.9rem;
-  border-radius: 4px;
-  justify-content: center;
-  align-items: center;
-  // position: absolute;
-  rigth: 50px;
-  :hover {
-    cursor: pointer;
-  }
-  position: relative;
-`;
-
-const NewTagText = styled.p`
-  color: white;
-  size: 1.5rem;
-  weight: bold;
-  // justify-content: center;
-  // align-items: center;
-  margin: auto;
-  width: 100%;
-  position: absolute;
-  left: 0.7rem;
-  top: 0.2rem;
-`;
-
-
+import {
+  DeviceCellContent,
+  NewTag,
+  NewTagText,
+  TableContainer,
+  TableStyled,
+  TdStyled,
+  ThStyled,
+  TitleStyled,
+} from "./suggestions.styles";
 
 export const SuggestionsTable = () => {
   const [suggestions, setSuggestions] = useState([]);
@@ -128,8 +28,10 @@ export const SuggestionsTable = () => {
   }, []);
 
   useEffect(() => {
-    updateSuggestions();
-  },[suggestions])
+    if (suggestions) {
+      updateSuggestions();
+    }
+  }, [suggestions]);
 
   return (
     <TableContainer>
@@ -140,7 +42,6 @@ export const SuggestionsTable = () => {
             <ThStyled>Device</ThStyled>
             <ThStyled>Suggested Rule</ThStyled>
             <ThStyled>Evidence</ThStyled>
-            {/* <ThStyled></ThStyled> */}
           </tr>
         </thead>
         <tbody>
