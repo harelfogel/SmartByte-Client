98f70523b59876ad57825e53ed46a155b744c45c
Yovel Elad <yovel@gmail.com>
2023-06-10 19:11:44 +0300

    adding voiced to pump

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,6 @@
 export const SERVER_URL = 'http://localhost:3001'
+//export const SERVER_URL = 'https://smartbute-alpha.onrender.com';
+
 
 export const TABLET_WIDTH = 1024;
 export const TABLET_HEIGHT = 768;
